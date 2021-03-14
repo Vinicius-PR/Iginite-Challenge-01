7b544ad56f2d2349d20304474d092a3af89cc580
Vinicius-PR <58795242+Vinicius-PR@users.noreply.github.com>
2021-03-14 16:20:05 -0300

    Update TaskList.tsx
    My solution.

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,8 +1,8 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
-import '../styles/tasklist.scss'
+import '../styles/tasklist.scss';
 
-import { FiTrash, FiCheckSquare } from 'react-icons/fi'
+import { FiTrash, FiCheckSquare } from 'react-icons/fi';
 
 interface Task {
   id: number;
@@ -16,14 +16,37 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (newTaskTitle === "") return;
+
+    const newTask = {
+      id:  Math.random(),
+      title: newTaskTitle,
+      isComplete: false,
+    }
+    const newArray = [...tasks, newTask];
+    setTasks(newArray);
+    setNewTaskTitle("");  
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const newTasks = tasks.map((task) => {
+      if (task.id === id) {
+        if (task.isComplete === false) {
+          task.isComplete = true;
+        } else {
+          task.isComplete = false;
+        }
+      }
+      return task;
+    });
+    setTasks(newTasks);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const newCollectionTasks = tasks.filter(task => task.id != id);
+    setTasks(newCollectionTasks);
   }
 
   return (
